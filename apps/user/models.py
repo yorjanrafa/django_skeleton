73b3687d4fcc4bc5483b48706b0a74ca0e8fb5d1
yorjansandoval <yorjan1999sandoval@gmail.com>
2024-03-04 16:23:52 -0400

    Initial commit

---
+++ apps/user/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# create abastract user model with image field
+
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', null=True, blank=True)
+
+
+# Create your models here.
+
+
